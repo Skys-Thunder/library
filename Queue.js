44f4718b7ed59ef5238faa6c9bab2e1d8764a909
Sky_Thunder <107789489+Thunder-Sky@users.noreply.github.com>
2023-08-03 22:13:19 +0900

    Update Queue.js

--- Queue.js
+++ Queue.js
@@ -2,8 +2,8 @@ function Queue(){
     this.data=new Array();
     let setting=Queue.prototype;
     setting.push=(x)=>this.data.push(x);
-    setting.pop=(x)=>this.data.shift(x);
-    setting.front=(x)=>this.data[0];
-    setting.size=(x)=>this.data.length;
-    setting.empty=(x)=>this.data.length==0;
+    setting.pop=()=>this.data.shift();
+    setting.front=()=>this.data[0];
+    setting.size=()=>this.data.length;
+    setting.empty=()=>this.data.length==0;
 }
