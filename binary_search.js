9e0e8c14621e799ae88ec545bf28cd0b48f1f755
Sky_Thunder <107789489+Thunder-Sky@users.noreply.github.com>
2023-08-10 14:49:32 +0900

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,10 @@
+function binary_search(arr,x){
+    let [L,R]=[0,arr.length-1];
+    while(L<=R){
+        M=Math.floor((L+R)/2);
+        if(arr[M]==x) return M;
+        if(arr[M]<x) L=M+1;
+        if(arr[M]>x) R=M-1;
+    }
+    return -1;
+}
