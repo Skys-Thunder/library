8142a20713d7209f3d6a9dc855d687aa99ace184
Sky_Thunder <107789489+Skys-Thunder@users.noreply.github.com>
2025-02-18 16:34:04 +0900

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,10 +1,28 @@
-function binary_search(arr,x){
-    let [L,R]=[0,arr.length-1];
-    while(L<=R){
-        let M=Math.floor((L+R)/2);
-        if(arr[M]==x) return M;
-        if(arr[M]<x) L=M+1;
-        if(arr[M]>x) R=M-1;
+function binary_search(arr,val){
+    let [l,r]=[0,arr.length-1];
+    while(l<=r){
+        let mid=0|(l+r)/2;
+        if(arr[mid]==val) return mid;
+        if(arr[mid]<val) l=mid+1;
+        if(arr[mid]>val) r=mid-1;
     }
     return -1;
+}
+function lower_bound(arr,val){
+    let [l,r,mid]=[0,arr.length-1,];
+    while(l<=r){
+        mid=0|(l+r)/2;
+        if(arr[mid]<val) l=mid+1;
+        else r=mid-1;
+    }
+    return l;
+}
+function upper_bound(arr,val){
+    let [l,r,mid]=[0,arr.length-1,];
+    while(l<=r){
+        mid=0|(l+r)/2;
+        if(arr[mid]<=val) l=mid+1;
+        else r=mid-1;
+    }
+    return l;
 }
