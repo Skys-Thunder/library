7b8252f66794884aff7ea8116d0f517278460863
Sky_Thunder <107789489+Skys-Thunder@users.noreply.github.com>
2025-05-21 23:32:13 +0900

    Update UnionFind.js

--- UnionFind.js
+++ UnionFind.js
@@ -1,19 +1,29 @@
-class UnionFind {
+class UnionFind{
     constructor(n){
+        this.n=n;
         this.parent=new Array(n).fill(-1);
-        this.leng=new Array(n).fill(1);
+        this.member=[...Array(n)].map(()=>new Set());
     }
-    issame(x,y){
-        return this.root(x)==this.root(y);
+    issame(a,b){ 
+        return this.root(a)==this.root(b);
     }
-    merge(x,y){
-        if(this.issame(x,y)) return;
-        let roots=[this.root(x),this.root(y)];
-        if(this.leng[roots[0]]>this.leng[roots[1]]) roots.reverse();
-        this.parent[roots[0]]=roots[1],this.leng[roots[1]]+=roots[0];
+    merge(a,b){
+        if(this.issame(a,b)) return;
+        let [x,y]=[this.root(a),this.root(b)];
+        if(-this.parent[x]>-this.parent[y]) [x,y]=[y,x];
+        this.parent[y]+=this.parent[x],this.parent[x]=y;
     }
-    root(x){
-        if(this.parent[x]==-1) return x;
-        return this.parent[x]=this.root(this.parent[x]);
+    root(a){
+        if(this.parent[a]<0) return a;
+        return this.parent[a]=this.root(this.parent[a]);
+    }
+    size(a){
+        return -this.parent[this.root(a)];
+    }
+    group(){
+        let res=[];
+        for(let v=0;v<this.n;v++) this.member[this.root(v)].add(v);
+        this.member.filter(n=>n.size!=0).map(z=>res.push([...z]));
+        return res;
     }
 }
